5e2e8d5f8b1b66362a9d86f516bbaa4f55991216
Felipe Cardozo <fcardozo@playvox.com>
2020-12-12 23:48:27 -0500

    Torre app icon.

--- components/layout.js
+++ components/layout.js
@@ -1,16 +1,15 @@
 import Head from 'next/head'
 import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
-
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       </Head>
       <main>{children}</main>
       {!home && (
